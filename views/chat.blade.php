2b65e193ec6ee0730fbbdb1f2436f3751d9c3499
Kyle Parisi <kyleparisi@gmail.com>
2019-06-09 15:46:32 -0400

    Fix chat component

--- views/chat.blade.php
+++ views/chat.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.marketing')
 
-@section('css')
-    <link rel="stylesheet" href="https://vue.kyleparisi.com/heffer.css">
-@endsection
-
 @section('content')
     <div class="w-80 fw6 lh-title center sans-serif">
         <a href="/" class="link black">< Home</a>
@@ -32,45 +28,4 @@
             <iframe src="/newsletter/subscribe" class="bn w-100 h5"></iframe>
         </div>
     </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="https://vue.kyleparisi.com/heffer.umd.min.js"></script>
-    <script>
-      $(function () {
-        lolight('pre');
-        new Vue({el: "#chat"});
-      });
-
-      function randomMessage() {
-        return faker.lorem.words(
-          faker.random.number({
-            min: 1,
-            max: 15
-          })
-        );
-      }
-      function fakeChat(key) {
-        return {
-          title: faker.random.words(5),
-          image_url: faker.image.imageUrl(),
-          owner: "1",
-          read: "0",
-          key,
-          users: {
-            "1": {
-              id: "1",
-              name: faker.name.findName(),
-              image_url: faker.image.imageUrl()
-            },
-            "2": {
-              id: "2",
-              name: faker.name.findName(),
-              image_url: faker.image.imageUrl()
-            }
-          },
-          messages: []
-        };
-      }
-    </script>
 @endsection
